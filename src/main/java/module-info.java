361685e556a99f88fa4891e6ace8cbf35c5fbf8a
bjberild <benjaminberild@outlook.com>
2025-02-26 12:59:45 +0100

    First Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module edu.ntnu.idi.idatt.mappeoppgavev2025 {
+  requires javafx.controls;
+  requires javafx.fxml;
+
+  requires org.controlsfx.controls;
+  requires org.kordamp.bootstrapfx.core;
+  requires com.almasb.fxgl.all;
+
+  opens edu.ntnu.idi.idatt.mappeoppgavev2025 to javafx.fxml;
+  exports edu.ntnu.idi.idatt.mappeoppgavev2025;
+}
