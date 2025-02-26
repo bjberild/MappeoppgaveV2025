dc4fe35ba0e91be610fd7d6fe7242dfb38a026d6
bjberild <benjaminberild@outlook.com>
2025-02-26 14:10:54 +0100

    First version of the Dice Class

--- src/main/java/edu/ntnu/idi/idatt/mappeoppgavev2025/Dice.java
+++ src/main/java/edu/ntnu/idi/idatt/mappeoppgavev2025/Dice.java
@@ -1,15 +1,26 @@
 package edu.ntnu.idi.idatt.mappeoppgavev2025;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class Dice {
-  private List<Die> dice;
+  private final ArrayList<Die> diceList = new ArrayList<>();
 
   public Dice(int numberOfDice) {
     for (int i = 0; i < numberOfDice; i++) {
-      dice.add(new Die());
+      diceList.add(new Die());
     }
+  }
 
+  public int rollAllDice() {
+    int sum = 0;
+    for (Die die : diceList) {
+      sum += die.roll();
+    }
+    return sum;
+  }
+
+  public int getDieValue(int dieIndex) {
+    return diceList.get(dieIndex).getValue();
   }
 
 }
