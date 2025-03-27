807139a9e08e0c0798237d2218613a806b654edc
bjberild <benjaminberild@outlook.com>
2025-03-27 08:30:56 +0100

    Undid changes to dice class

--- src/main/java/edu/ntnu/idi/idatt/mappeoppgavev2025/Dice.java
+++ src/main/java/edu/ntnu/idi/idatt/mappeoppgavev2025/Dice.java
@@ -1,15 +1,24 @@
 package edu.ntnu.idi.idatt.mappeoppgavev2025;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Dice {
-  private List<Die> dice;
+  private final List<Die> dice;
 
   public Dice(int numberOfDice) {
+    dice = new ArrayList<>();
     for (int i = 0; i < numberOfDice; i++) {
       dice.add(new Die());
     }
+  }
 
+  public int roll() {
+    int sum = 0;
+    for (Die die : dice) {
+      sum += die.roll();
+    }
+    return sum;
   }
 
 }
