d68c0890e7753a770b1bd5599ae538a1bc1329d7
Stian <stiando@stud.ntnu.no>
2025-05-17 18:27:22 +0200

    feat: Implement new TileAction classes for FallTrap and Portal with execute method

---
+++ src/main/java/edu/ntnu/idi/idatt/mappeoppgavev2025/model/FallTrapAction.java
@@ -0,0 +1,12 @@
+package edu.ntnu.idi.idatt.mappeoppgavev2025.model;
+
+public class FallTrapAction implements TileAction {
+    private final Tile destination;
+    public FallTrapAction(Tile destination) {
+        this.destination = destination;
+    }
+    @Override
+    public void execute(Player player) {
+        player.setCurrentTile(destination);
+    }
+}
---
+++ src/main/java/edu/ntnu/idi/idatt/mappeoppgavev2025/model/PortalAction.java
@@ -0,0 +1,14 @@
+package edu.ntnu.idi.idatt.mappeoppgavev2025.model;
+
+public class PortalAction implements TileAction {
+
+    private final Tile destination;
+    public PortalAction(Tile destination) {
+        this.destination = destination;
+    }
+
+    @Override
+    public void execute(Player player) {
+        player.setCurrentTile(destination);
+    }
+}
