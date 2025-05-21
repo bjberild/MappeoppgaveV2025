d92a8e154604ee682c43b31da5a38a24220b8ec5
Stian <stiando@stud.ntnu.no>
2025-05-21 15:56:02 +0200

    refactor: Update token image loading logic in ResourceTokenIconFactory for better error handling and path management

--- src/main/java/edu/ntnu/idi/idatt/mappeoppgavev2025/view/tokens/ResourceTokenIconFactory.java
+++ src/main/java/edu/ntnu/idi/idatt/mappeoppgavev2025/view/tokens/ResourceTokenIconFactory.java
@@ -8,9 +8,20 @@ import javafx.scene.image.ImageView;
 public class ResourceTokenIconFactory implements TokenIconFactory {
     @Override
     public ImageView createIcon(String tokenName) {
-        String path = "edu/ntnu/idi/idatt/mappeoppgavev2025/resources/tokens/" + tokenName + ".png";
+        String path = "/resources/edu/ntnu/idi/idatt/mappeoppgavev2025/images/tokens/" + tokenName + ".png";
         InputStream in = getClass().getClassLoader().getResourceAsStream(path);
-        if (in == null) throw new IllegalArgumentException("Token not found: " + path);
+        if (in == null) {
+            in = getClass().getResourceAsStream("/edu/ntnu/idi/idatt/mappeoppgavev2025/images/tokens/" + tokenName + ".png");
+        }
+
+        if (in == null) {
+            in = getClass().getResourceAsStream("/edu/ntnu/idi/idatt/mappeoppgavev2025/images/tokens/DefaultToken.png");
+        }
+
+        if (in == null) {
+            throw new IllegalArgumentException("Token not found: " + path);
+        }
+        
         ImageView iv = new ImageView(new Image(in));
         iv.setFitWidth(30);
         iv.setFitHeight(30);
