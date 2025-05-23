41c53504c99a224ef8ac37fb059277e6870680d3
Stian <stiando@stud.ntnu.no>
2025-05-23 07:38:58 +0200

    ``` feat: Integrate Snakes and Ladders game view into MainMenuGUI for enhanced user experience ```

--- src/main/java/edu/ntnu/idi/idatt/mappeoppgavev2025/view/SnakesAndLaddersView.java
+++ src/main/java/edu/ntnu/idi/idatt/mappeoppgavev2025/view/SnakesAndLaddersView.java
@@ -1,16 +1,27 @@
 package edu.ntnu.idi.idatt.mappeoppgavev2025.view;
 
+
 import edu.ntnu.idi.idatt.mappeoppgavev2025.controller.PlayerController;
 import edu.ntnu.idi.idatt.mappeoppgavev2025.view.board.DefaultPipPlacementStrategy;
 import edu.ntnu.idi.idatt.mappeoppgavev2025.view.board.DefaultTileCellFactory;
 import edu.ntnu.idi.idatt.mappeoppgavev2025.view.tokens.ResourceTokenIconFactory;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 
 public class SnakesAndLaddersView extends GameView {
     private BoardView boardView;
+    private final Button returnButton = new Button("← Back to Main Menu");
 
     public SnakesAndLaddersView(PlayerController ctrl) {
         super(ctrl);
+        Node oldBottom = getBottom();
+        var bottomBox = new VBox(10, oldBottom, returnButton);
+        bottomBox.setPadding(new Insets(10));
+        setBottom(bottomBox);
     }
     @Override
     protected Node createBoardPane() {
@@ -32,5 +43,9 @@ public class SnakesAndLaddersView extends GameView {
 
     public BoardView getBoardView() {
         return boardView;
+    }
+
+    public void setOnReturn(EventHandler<ActionEvent> handler) {
+        returnButton.setOnAction(handler);
     }
 }
